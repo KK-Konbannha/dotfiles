82869b21b789799b7f0c2c648b418ced0571892e
KK-Konbannha <katsuk.618@gmail.com>
2022-05-07 10:45:57 +0900

    fix fc-cache option and ghcup-env

--- install.sh
+++ install.sh
@@ -299,7 +299,7 @@ initialize() {
     curl -LO https://github.com/yuru7/HackGen/releases/download/v2.6.3/HackGenNerd_v2.6.3.zip
     unzip HackGenNerd_v2.6.3.zip
     cp -r HackGenNerd_v2.6.3 ${HOME}/.local/share/fonts/
-    fc-cache -ve
+    fc-cache -vf
     cd ${HOME}
 
     # vim-plug
