0148ed66514b4ef70e399feb8c7dd887a7e095fe
KK-Konbannha <katsuk.618@gmail.com>
2022-05-11 10:29:56 +0900

    update qtile config

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -52,23 +52,10 @@ keys = [
     Key([mod, "shift"], "d", lazy.layout.shuffle_right(), desc="Move window to the right"),
     Key([mod, "shift"], "s", lazy.layout.shuffle_down(), desc="Move window down"),
     Key([mod, "shift"], "w", lazy.layout.shuffle_up(), desc="Move window up"),
-    # Grow windows. If current window is on the edge of screen and direction
-    # will be to screen edge - window would shrink.
-    Key([mod, "control"], "a", lazy.layout.grow_left(), desc="Grow window to the left"),
-    Key([mod, "control"], "d", lazy.layout.grow_right(), desc="Grow window to the right"),
-    Key([mod, "control"], "s", lazy.layout.grow_down(), desc="Grow window down"),
-    Key([mod, "control"], "w", lazy.layout.grow_up(), desc="Grow window up"),
-    Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
     # Toggle between split and unsplit sides of stack.
     # Split = all windows displayed
     # Unsplit = 1 window displayed, like Max layout, but still with
     # multiple stack panes
-    Key(
-        [mod, "shift"],
-        "Return",
-        lazy.layout.toggle_split(),
-        desc="Toggle between split and unsplit sides of stack",
-    ),
     Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
     # Toggle between different layouts as defined below
     # Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
