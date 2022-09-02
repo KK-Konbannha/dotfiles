4d12fe2ac320649663dca1a373559572e6dcabb4
KK-Konbannha <katsuk.618@gmail.com>
2022-09-02 16:18:19 +0900

    add wezterm conf

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,72 @@
+local wezterm = require("wezterm")
+
+local function font_with_fallback(name, params)
+  local names = { name, "Apple Color Emoji", "HackGenNerd Console" }
+  return wezterm.font_with_fallback(names, params)
+end
+
+local font_name = "HackGenNerd With FFxiv Glyph"
+
+return {
+  front_end = "OpenGL",
+  enable_wayland = false,
+
+  color_scheme = 'Dracula (Official)',
+
+  font = font_with_fallback(font_name),
+  font_rules = {
+    {
+      italic = true,
+      font = font_with_fallback(font_name, { italic = true }),
+    },
+    {
+      italic = true,
+      intensity = "Bold",
+      font = font_with_fallback(font_name, { italic = true, bold = true }),
+    },
+    {
+      intensity = "Bold",
+      font = font_with_fallback(font_name, { bold = true }),
+    },
+  },
+  warn_about_missing_glyphs = false,
+  font_size = 24,
+  line_height = 1.0,
+
+  enable_tab_bar = true,
+  hide_tab_bar_if_only_one_tab = true,
+  show_tab_index_in_tab_bar = false,
+  tab_bar_at_bottom = true,
+
+  window_padding = {
+    left = 20,
+    right = 4,
+    top = 20,
+    bottom = 10,
+  },
+
+  disable_default_key_bindings = true,
+  keys = {
+    {
+      key = "y",
+      mods = "CTRL|SHIFT",
+      action = "ActivateCopyMode",
+    },
+    {
+      key = "v",
+      mods = "CTRL|SHIFT",
+      action = wezterm.action({ PasteFrom = "Clipboard" }),
+    },
+    {
+      key = "c",
+      mods = "CTRL|SHIFT",
+      action = wezterm.action({ CopyTo = "ClipboardAndPrimarySelection" }),
+    },
+  },
+
+
+  audible_bell = "Disabled",
+  automatically_reload_config = true,
+  window_background_opacity = 0.7,
+  window_close_confirmation = "NeverPrompt",
+}
--- README.md
+++ README.md
@@ -3,9 +3,8 @@
 ## Information
 - **OS:** [Arch Linux](https://archlinux.org)
 - **WM:** [qtile](https://github.com/qtile/qtile)
-- **Terminal:** [wezterm](https://github.com/wez/wezterm)
+- **Terminal:** [Alacritty](https://github.com/alacritty/alacritty) / [wezterm](https://github.com/wez/wezterm)
 - **Shell:** [zsh](https://www.zsh.org/)
 - **Editor:** [neovim](https://github.com/neovim/neovim)
 - **Compositor:** [picom](https://github.com/yshui/picom)
 - **Application Launcher:** [rofi](https://github.com/davatorium/rofi)
-- **Music Player** [ncmpcpp](https://github.com/ncmpcpp/ncmpcpp)
