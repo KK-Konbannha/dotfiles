6306a2d4883a705ee1cbf0d9209f2056cae26c8f
KK-Konbannha <katsuk.618@gmail.com>
2023-08-03 14:17:04 +0900

    update

--- .config/nvim/after/plugin/comment-box.rc.lua
+++ .config/nvim/after/plugin/comment-box.rc.lua
@@ -1,11 +1,11 @@
-local status, cb = pcall(require, "comment-box")
-if (not status) then return end
-
-local keymap = vim.keymap.set
-
--- left aligned fixed size box with left aligned text
-keymap({ "n", "v" }, "<Leader>bb", cb.cbox, {})
--- centered adapted box with centered text
-keymap({ "n", "v" }, "<Leader>bc", cb.accbox, {})
--- left aligned adapted box with centered text
-keymap({ "n", "v" }, "<Leader>cc", cb.acbox, {})
+-- local status, cb = pcall(require, "comment-box")
+-- if (not status) then return end
+-- 
+-- local keymap = vim.api.nvim_set_keymap
+-- 
+-- -- left aligned fixed size box with left aligned text
+-- keymap({ "n", "v" }, "<Leader>bb", cb.cbox, {})
+-- -- centered adapted box with centered text
+-- keymap({ "n", "v" }, "<Leader>bc", cb.accbox, {})
+-- -- left aligned adapted box with centered text
+-- keymap({ "n", "v" }, "<Leader>cc", cb.acbox, {})
