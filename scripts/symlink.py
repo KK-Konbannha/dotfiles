7b8d9faf70e405e9abef0dc149d1fd9a23f90a90
KK-Konbannha <katsuk.618@gmail.com>
2023-08-25 16:06:46 +0900

    test

--- scripts/symlink.py
+++ scripts/symlink.py
@@ -55,6 +55,8 @@ def symlink_dotfiles(
                     return False
                 print(b"/home" in res.stdout)
                 print(b"failed" in res.stdout)
+                pritn(b"failed")
+                print(res.stdout)
                 sys.stdout.buffer.write(res.stdout)
                 if b"failed" in res.stdout:
                     return False
