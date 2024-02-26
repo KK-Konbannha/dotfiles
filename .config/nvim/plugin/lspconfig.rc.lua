7e8c2059e773a150809eab3bcac47e4501a941fa
KK-Konbannha <katsuk.618@gmail.com>
2024-02-27 08:55:57 +0900

    update none-ls config and lspconfig

--- .config/nvim/plugin/lspconfig.rc.lua
+++ .config/nvim/plugin/lspconfig.rc.lua
@@ -3,9 +3,47 @@ if not status then
 	return
 end
 
-lspconfig.pyright.setup({})
 lspconfig.tsserver.setup({})
+lspconfig.eslint.setup({
+	on_attach = function(client, bufnr)
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			bufnr = bufnr,
+			command = "EslintFixAll",
+		})
+	end,
+})
 lspconfig.jdtls.setup({})
+lspconfig.pyright.setup({
+	settings = {
+		pyright = {
+			disableOrganizeImports = true,
+		},
+		python = {
+			analysis = {
+				ignore = { "*" },
+			},
+		},
+	},
+})
+--
+lspconfig.ruff_lsp.setup({
+	on_attach = function(client, bufnr)
+		client.server_capabilities.hoverProvider = false
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					vim.lsp.buf.format()
+				end,
+			})
+		end
+	end,
+	settings = {
+		args = {},
+	},
+})
 lspconfig.clangd.setup({
 	cmd = {
 		"clangd",
