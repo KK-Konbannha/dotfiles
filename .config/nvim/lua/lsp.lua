38b4d23b73bac583315ff07d4e9ebedb23b1710c
KK-Konbannha <katsuk.618@gmail.com>
2025-05-22 10:43:56 +0900

    update

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -1,2 +1,4 @@
 vim.lsp.enable("pyright")
 vim.lsp.enable("ruff")
+vim.lsp.enable("rust_analyzer")
+vim.lsp.enable("clangd")
--- .config/nvim/plugin/noice.rc.lua
+++ .config/nvim/plugin/noice.rc.lua
@@ -5,11 +5,12 @@ end
 
 noice.setup({
 	lsp = {
-		-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-		override = {
-			["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-			["vim.lsp.util.stylize_markdown"] = true,
-			["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+		hover = {
+			enabled = false,
+			silent = true,
+		},
+		signature = {
+			enabled = false,
 		},
 	},
 	presets = {
